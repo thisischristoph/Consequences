bdef69b6561b4c72f0a778fd408bd4355dd7ad23
Chris <christopher.harrison.2011@gmail.com>
2017-11-20 23:43:26 +0000

    Changed Animation Method

--- Consequences/PackCollectionViewController.swift
+++ Consequences/PackCollectionViewController.swift
@@ -23,6 +23,8 @@ class PackCollectionViewController: UICollectionViewController {
     let previewPack = UIImageView()
     var animating = false
     let blurTapRecognizer = UITapGestureRecognizer()
+    var absoluteForgroundFrame = CGPoint()
+    var tempCellRect = CGRect()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -69,18 +71,14 @@ class PackCollectionViewController: UICollectionViewController {
             let cellScreenPercentage = (((((absoluteframe?.y)! + (cellSize.height/2)) / (screenSize.size.height))*100)-50) * CGFloat(shiftMultiplier)
             cell.foregroundPack.center.y = cell.backgroundPack.center.y + cellScreenPercentage
         }
-        
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("section: \(indexPath.section)")
         print("row: \(indexPath.row)")
         let cell = collectionView.cellForItem(at: indexPath) as! PackCollectionViewCell
-        
         cellRecordIndexPath = indexPath
-        
-        let absoluteForgroundFrame = cell.foregroundPack.superview?.convert(cell.foregroundPack.frame.origin, to: nil)
-        
+
         //Add mask
         blurEffectView.effect = blurEffect
         blurEffectView.frame = self.view.bounds
@@ -102,7 +100,9 @@ class PackCollectionViewController: UICollectionViewController {
         
         
         // Add Cell Copy
-        previewPack.frame = CGRect(x: (absoluteForgroundFrame?.x)!, y: (absoluteForgroundFrame?.y)!, width: cell.foregroundPack.frame.width, height: cell.foregroundPack.frame.height)
+        absoluteForgroundFrame = (cell.foregroundPack.superview?.convert(cell.foregroundPack.frame.origin, to: nil))!
+        tempCellRect = CGRect(x: absoluteForgroundFrame.x, y: absoluteForgroundFrame.y, width: cell.foregroundPack.frame.width, height: cell.foregroundPack.frame.height)
+        previewPack.frame = tempCellRect
         previewPack.layer.cornerRadius = cell.foregroundPack.layer.cornerRadius
         previewPack.backgroundColor = .green
         collectionView.superview?.addSubview(previewPack)
@@ -110,40 +110,40 @@ class PackCollectionViewController: UICollectionViewController {
         //Hide cell behind
         cell.backgroundPack.isHidden = true
         cell.foregroundPack.isHidden = true
+        self.disableUserInteraction()
+        self.animating = true
         
-        //Animate new image
-        UIView.animate(withDuration: 0.6, animations: {
-            self.disableUserInteraction()
-            self.animating = true
-            self.previewPack.transform = CGAffineTransform.identity.translatedBy(x: (self.previewPack.superview?.center.x)! - self.previewPack.center.x, y: (self.previewPack.superview?.center.y)! - self.previewPack.center.y).scaledBy(x: 2, y: 2)
-        }, completion: {
-            (value: Bool) in
+        let animation = UIViewPropertyAnimator(duration: 0.6, curve: .easeInOut)
+        animation.addAnimations {
+            self.previewPack.center.x = screenSize.width/2
+            self.previewPack.center.y = screenSize.height/2
+            self.previewPack.transform = CGAffineTransform(scaleX: 2, y: 2)
+        }
+        animation.addCompletion {_ in
             self.animating = false
             self.enableUserInteraction()
-        })
+            
+        }
+        animation.startAnimation()
     }
     
     @objc func reverseCellSelect(_ sender: UITapGestureRecognizer) {
         print("Working")
         let cell = collectionView?.cellForItem(at: self.cellRecordIndexPath) as! PackCollectionViewCell
-        let absoluteForgroundFrame = cell.foregroundPack.superview?.convert(cell.foregroundPack.frame.origin, to: nil)
         
-        UIView.animate(withDuration: 0.6, animations: {
-            self.previewPack.transform = CGAffineTransform.identity.translatedBy(x: (absoluteForgroundFrame?.x)! - (self.previewPack.superview?.center.x)!, y: (absoluteForgroundFrame?.y)! -  (self.previewPack.superview?.center.y)!).scaledBy(x: 1, y: 1github
-            
-            )
-        }, completion: {
-            (value: Bool) in
-            //show cell behind
+        let animation = UIViewPropertyAnimator(duration: 0.6, curve: .easeInOut)
+        animation.addAnimations {
+            self.previewPack.frame = self.tempCellRect
+            self.blurEffectView.alpha = 0
+            self.previewPack.transform = CGAffineTransform.identity
+        }
+        animation.addCompletion {_ in
             cell.backgroundPack.isHidden = false
             cell.foregroundPack.isHidden = false
-            UIView.animate(withDuration: 0.3, animations: {
-                self.blurEffectView.alpha = 0
-            }, completion: {
-                (value: Bool) in
-                self.blurEffectView.removeFromSuperview()
-            })
-        })
+            self.blurEffectView.removeFromSuperview()
+            self.previewPack.removeFromSuperview()
+        }
+        animation.startAnimation()
     }
     
     func disableUserInteraction() {
@@ -154,6 +154,10 @@ class PackCollectionViewController: UICollectionViewController {
     func enableUserInteraction(){
         self.blurEffectView.isUserInteractionEnabled = true
         self.previewPack.isUserInteractionEnabled = true
+    }
+    
+    func addPreviewPack(){
+        
     }
     
 }
